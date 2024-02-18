c7ab01375de48a4e85dffe13f814b4a8930cc015
fai5121988@yahoo.com.hk <fai5121988@yahoo.com.hk>
2024-02-18 05:01:24 +0000

    handled the case when lidar beam is blocked by parallel wall

--- pose_graph_slam/map.py
+++ pose_graph_slam/map.py
@@ -29,20 +29,21 @@ class Lidar():
 
     def __init__(self, c):
         self.c = c
+        self.min_range = 0.1
         self.max_range = 5.
         self.max_ang_range = np.math.pi*2.
         self.resol = np.deg2rad(2.)
         self.N_beams = int(self.max_ang_range/self.resol)
         self.beam_list = [None]*self.N_beams
-        print(self.N_beams)
+        # self.min_beam_list = [None]*self.N_beams
 
-    def emit(self):
+    def emit(self, heading):
         
         emit_ang = np.linspace(0., 2*np.pi - self.resol, self.N_beams)
-        print(emit_ang[0], np.rad2deg(emit_ang[-1]), np.rad2deg(emit_ang[1] - emit_ang[0]))
 
-        xs = np.cos(emit_ang)*(-self.max_range) - np.sin(emit_ang)*0. + self.c.x
-        ys = np.sin(emit_ang)*(-self.max_range) + np.cos(emit_ang)*0. + self.c.y
+        theta = emit_ang + heading
+        xs = np.cos(theta)*(self.max_range) - np.sin(theta)*0. + self.c.x
+        ys = np.sin(theta)*(self.max_range) + np.cos(theta)*0. + self.c.y
 
         cnt = 0
         for x, y in zip(xs, ys):
@@ -77,7 +78,7 @@ def check_intersect(line1, line2):
     b = np.array([[line2.P0.x - line1.P0.x], 
                   [line2.P0.y - line1.P0.y]])
     
-    if np.linalg.det(A) != 0.:
+    if abs(np.linalg.det(A)) >= 1e-5:
         return np.matmul(np.linalg.inv(A), b)
     else:
         return 
@@ -124,29 +125,35 @@ O_pt = Pt(5, 10)
 S_pt, E_pt = O_pt, O_pt.add(0, -1.5)
 map.add_line(S_pt, E_pt)
 
-# map.plot()
-# l1 = Line(Pt(0, 0), Pt(0, 10))
-# l2 = Line(Pt(5, 5), Pt(0,  5))
-# intersect = check_intersect(l1, l2)
-
-# print(intersect[0]*l1.u + np.array([l1.P0.x, l1.P0.y]))
-# print(intersect[1]*l2.u + np.array([l2.P0.x, l2.P0.y]))
-
-lidar = Lidar(Pt(8, 5))
-lidar.emit()
+heading = np.deg2rad(200)
+lidar = Lidar(Pt(3, 8))
+lidar.emit(heading)
 
 intersect_list = [None]*len(lidar.beam_list)
 for i, b in enumerate(lidar.beam_list):
     best_d = np.inf
     for ml in map.line_list:
+        
+        #check if any intersection
         intersect = check_intersect(ml, b)
-        if intersect is not None and (intersect[1] < best_d and intersect[1] > 0. and abs(intersect[1]) <= b.L) and (intersect[0] > 0. and abs(intersect[0]) <= ml.L):
+
+        if intersect is None:
+            if (b.P0.x == ml.P0.x or b.P0.y == ml.P0.y):
+                # beam is parallel to a wall => completely blocked => break
+                best_d = np.inf #ensure no incorrect sensing
+                break
+            #just parallel but not blocked by a wall => continue
+            continue
+
+        is_within_range = (intersect[1] >= lidar.min_range and intersect[1] <= b.L)
+        is_block = intersect[1] < best_d
+        is_within_wall  = (intersect[0] >= 0. and intersect[0] <= ml.L) 
+
+        if is_block and is_within_range and is_within_wall:
             best_d = intersect[1]
         
     if best_d is not np.inf:    
-        
         intersect_list[i] = best_d*b.u + np.array([b.P0.x, b.P0.y])
-        print(best_d, intersect_list[i])
 
 ax = plt.subplot(1, 1, 1)
 
@@ -155,8 +162,14 @@ for line in map.line_list:
             [line.pt1.y, line.pt2.y], '-b')
 
 for line in lidar.beam_list:
-    ax.plot([line.pt1.x, line.pt2.x],
-            [line.pt1.y, line.pt2.y], '--r')
+    ax.plot(line.pt2.x, line.pt2.y, '.r')
+
+ax.plot([lidar.beam_list[0].pt1.x, lidar.beam_list[0].pt2.x], 
+        [lidar.beam_list[0].pt1.y, lidar.beam_list[0].pt2.y], '--r')
+ax.plot([lidar.c.x, lidar.c.x + np.cos(heading)],
+        [lidar.c.y, lidar.c.y + np.sin(heading)], '-y')
+
+ax.plot(lidar.c.x, lidar.c.y, '+r')
 
 for pt in intersect_list:
     if pt is not None:
